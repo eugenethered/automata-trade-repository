26a15e8987a7ff007224d563e8b74908f5b9c1cb
Eugene the Red <me@eugenethered.com>
2022-05-17 13:56:57 +0100

    added trade history (via abstraction)

--- tests/traderepo/repository/TradeRepository_test.py
+++ tests/traderepo/repository/TradeRepository_test.py
@@ -20,12 +20,48 @@ class TradeRepositoryTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.cache.delete('test:trade')
+        self.cache.delete('test:trade:history')
 
     def test_should_store_and_retrieve_trade(self):
-        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.SUBMITTED, order_id='88888888', instant=1)
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.SUBMITTED, order_id='8888-8888', instant=1)
         self.repository.store_trade(trade)
         stored_trade = self.repository.retrieve_trade()
         self.assertEqual(trade, stored_trade)
+
+    def test_should_store_executed_trade_on_historic_trades_list(self):
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.EXECUTED, order_id='8888-8888', instant=1)
+        self.repository.store_trade(trade)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 1)
+
+    def test_should_not_store_non_executed_trade_on_historic_trades_list(self):
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.SUBMITTED, order_id='8888-8888', instant=1)
+        self.repository.store_trade(trade)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+
+    def test_should_not_store_trade_without_order_id_on_historic_trades_list(self):
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.EXECUTED, instant=1)
+        self.repository.store_trade(trade)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+
+    def test_should_not_store_same_executed_trade_on_historic_trades_list(self):
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.EXECUTED, order_id='8888-8888', instant=1)
+        self.repository.store_trade(trade)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 1)
+        # try string the same trade again (based on equality) in history
+        other = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('200.00'), price=BigFloat('2.02'), value=BigFloat('404'), status=Status.EXECUTED, order_id='8888-8888', instant=1)
+        self.repository.store_trade(other)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 1)
 
 
 if __name__ == '__main__':
