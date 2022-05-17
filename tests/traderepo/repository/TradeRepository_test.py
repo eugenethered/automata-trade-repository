9e4bdb9df72b1c88be8c5bc12cc5d254867afd3d
Eugene the Red <me@eugenethered.com>
2022-05-17 14:14:54 +0100

    added optional limit store

--- tests/traderepo/repository/TradeRepository_test.py
+++ tests/traderepo/repository/TradeRepository_test.py
@@ -10,13 +10,14 @@ from traderepo.repository.TradeRepository import TradeRepository
 class TradeRepositoryTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        options = {
+        self.options = {
             'REDIS_SERVER_ADDRESS': '192.168.1.90',
             'REDIS_SERVER_PORT': 6379,
-            'TRADE_KEY': 'test:trade'
+            'TRADE_KEY': 'test:trade',
+            'TRADE_HISTORY_LIMIT': 2
         }
-        self.cache = RedisCacheHolder(options)
-        self.repository = TradeRepository(options)
+        self.cache = RedisCacheHolder(self.options)
+        self.repository = TradeRepository(self.options)
 
     def tearDown(self):
         self.cache.delete('test:trade')
@@ -62,6 +63,32 @@ class TradeRepositoryTestCase(unittest.TestCase):
         self.repository.store_trade(other)
         historic_trades = self.repository.retrieve_historic_trades()
         self.assertTrue(len(historic_trades) == 1)
+
+    def test_should_store_limited_executed_trades_on_historic_trades_list(self):
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.EXECUTED, order_id='8888-8888', instant=1)
+        self.repository.store_trade(trade)
+        trade.order_id = '2222-2222'
+        self.repository.store_trade(trade)
+        trade.order_id = '3333-3333'
+        self.repository.store_trade(trade)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 2)
+
+    def test_should_not_limit_store_executed_trades_on_historic_trades_list(self):
+        del self.options['TRADE_HISTORY_LIMIT']
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 0)
+        trade = InstrumentTrade(instrument_from='OTC', instrument_to='GBP', quantity=BigFloat('100.00'), price=BigFloat('1.01'), value=BigFloat('101'), status=Status.EXECUTED, order_id='8888-8888', instant=1)
+        self.repository.store_trade(trade)
+        trade.order_id = '2222-2222'
+        self.repository.store_trade(trade)
+        trade.order_id = '3333-3333'
+        self.repository.store_trade(trade)
+        historic_trades = self.repository.retrieve_historic_trades()
+        self.assertTrue(len(historic_trades) == 3)
+
 
 
 if __name__ == '__main__':
--- traderepo/repository/TradeRepository.py
+++ traderepo/repository/TradeRepository.py
@@ -7,6 +7,7 @@ from traderepo.repository.serialize.trade_deserializer import deserialize_trade
 from traderepo.repository.serialize.trade_serializer import serialize_trade
 
 TRADE_KEY = 'TRADE_KEY'
+TRADE_HISTORY_LIMIT = 'TRADE_HISTORY_LIMIT'
 
 
 class TradeRepository:
@@ -22,39 +23,45 @@ class TradeRepository:
         if TRADE_KEY not in self.options:
             raise MissingOptionError(f'missing option please provide option {TRADE_KEY}')
 
-    def build_trade_key(self):
+    def __build_trade_key(self):
         return self.options[TRADE_KEY]
 
-    def build_historic_trades_key(self):
-        trade_key = self.build_trade_key()
+    def __build_historic_trades_key(self):
+        trade_key = self.__build_trade_key()
         return f'{trade_key}:history'
 
     def store_trade(self, trade: InstrumentTrade):
-        trade_key = self.build_trade_key()
+        trade_key = self.__build_trade_key()
         trade_to_store = serialize_trade(trade)
         self.cache.store(trade_key, trade_to_store)
         self.store_historical_trade(trade)
 
     def retrieve_trade(self) -> InstrumentTrade:
-        trade_key = self.build_trade_key()
+        trade_key = self.__build_trade_key()
         raw_trade = self.cache.fetch(trade_key, as_type=dict)
         return deserialize_trade(raw_trade)
 
     def store_historical_trade(self, trade: InstrumentTrade):
         if trade.status == Status.EXECUTED and is_empty(trade.order_id) is False:
             historical_trades = self.retrieve_historic_trades()
-            if self.is_already_history(trade, historical_trades) is False:
+            if self.__is_already_history(trade, historical_trades) is False:
                 historical_trades.append(trade)
-                entities_to_store = list([serialize_trade(trade) for trade in historical_trades])
-                key = self.build_historic_trades_key()
-                self.cache.store(key, entities_to_store)
+                self.__store_historical_trades_with_limit(historical_trades)
 
     @staticmethod
-    def is_already_history(trade, historical_trades):
+    def __is_already_history(trade, historical_trades):
         matching_trades = list([ht for ht in historical_trades if ht == trade])
         return len(matching_trades) > 0
 
+    def __store_historical_trades_with_limit(self, historical_trades):
+        entities_to_store = list([serialize_trade(trade) for trade in historical_trades])
+        if TRADE_HISTORY_LIMIT in self.options:
+            if len(entities_to_store) > int(self.options[TRADE_HISTORY_LIMIT]):
+                entities_to_store = entities_to_store[1:]
+        key = self.__build_historic_trades_key()
+        self.cache.store(key, entities_to_store)
+
     def retrieve_historic_trades(self):
-        key = self.build_historic_trades_key()
+        key = self.__build_historic_trades_key()
         raw_entities = self.cache.fetch(key, as_type=list)
         return list([deserialize_trade(raw) for raw in raw_entities])
